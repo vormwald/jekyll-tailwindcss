43380e92ac194455ef8c58475a764ebccd8871d5
Mike Vormwald <mvormwald@gmail.com>
2024-02-12 14:25:11 +0000

    bundle gem jekyll-tailwindcss

---
+++ spec/jekyll/tailwindcss_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Jekyll::Tailwindcss do
+  it "has a version number" do
+    expect(Jekyll::Tailwindcss::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

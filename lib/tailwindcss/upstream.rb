29c0ff15b4b37b5967355b24dd15e65b1ed52885
Mike Vormwald <mvormwald@gmail.com>
2025-01-04 22:53:47 -0500

    remove this unused file

--- lib/tailwindcss/upstream.rb
+++
@@ -1,7 +0,0 @@
-module Tailwindcss
-  # constants describing the upstream tailwindcss project
-  module Upstream
-    VERSION = Tailwindcss::Ruby::Upstream::VERSION
-    deprecate_constant :VERSION
-  end
-end
